c9f3c05f3ce8726eef7afc0446c4787fbfcf310e
Michael Amoabeng <michael.amoabeng2015@gmail.com>
2017-05-18 00:53:12 +0300

    vue-resource package has been added and configured. This is an external package for using an external RESTful API

--- news-app/src/main.js
+++ news-app/src/main.js
@@ -2,6 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueResource from 'vue-resource'
+
+//Attach vue-resource plugin to the vue instance
+Vue.use(VueResource);
 
 Vue.config.productionTip = false
 
